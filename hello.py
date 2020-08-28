1336195749766c6fc6bc01fce6b19fda3bb4910b
Skorpion7777 <s.pirozhkov@hotmail.com>
2020-08-28 18:37:46 +0200

    Use scatter plot, index = order instead of 'name'

--- hello.py
+++ hello.py
@@ -2,12 +2,12 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-presidents_df = pd.read_csv('https://sololearn.com/uploads/files/president_heights_party.csv', index_col='name')
+presidents_df = pd.read_csv('https://sololearn.com/uploads/files/president_heights_party.csv', index_col='order')
 
 print(presidents_df.head())
 
 #Single Indexing
-presidents_df.loc['Abraham Lincoln']
+presidents_df.loc[5]
 
 #Multiple Indexing
 presidents_df.loc['John Tyler':'Abraham Lincoln']
@@ -29,10 +29,11 @@ age_quartiles = presidents_df['age'].quantile([0.25,0.5,0.75,1])
 age_quartiles = np.array(age_quartiles)
 print(age_quartiles)
 
-presidents_names = presidents_df['order']
-print(presidents_names)
 #Plot Age
 fig = plt.figure()
+fig.set_size_inches(18.5, 10.5, forward=True)
 ax1 = fig.add_subplot(111)
-ax1.plot(presidents_df['order'], presidents_df['age'])
+ax1.scatter(presidents_df['name'], presidents_df['age'])
+plt.xticks(rotation=90)
+ax1.tick_params(axis='x', length=15, labelsize=14)
 plt.show()
